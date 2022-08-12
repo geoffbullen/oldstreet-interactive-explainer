1ca5200d1dd0d8b40fdab3b3c04184ead75244d5
Jeff Zhang <jeff@wallsync.net>
2022-08-12 15:58:44 +1000

    tabs implementation

---
+++ components/pseudo-tab.js
@@ -0,0 +1,37 @@
+class PseudoTab extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        let activeTab = this.getAttribute('active')
+
+        const isActive = (tabName) => {
+            if (activeTab === tabName) {
+                return 'pseudo-tab-active'
+            } else {
+                return ''
+            }
+        }
+
+        const addOnClickListener = (tabName) => {
+            if (activeTab === tabName) {
+                return ''
+            } else {
+                return `onclick="window.location.href=\'/${tabName}\'"`
+            }
+        }
+
+        this.innerHTML = `
+            <div class="pseudo-tabs-container">
+                <button class="pseudo-tab-button ${isActive('chart-types')}"
+                    ${addOnClickListener('chart-types')}>Chart types</button>
+                <button class="pseudo-tab-button ${isActive('features')}"
+                    ${addOnClickListener('features')}>Other features</button>
+                <button class="pseudo-tab-button ${isActive('editor')}"
+                    ${addOnClickListener('editor')}>Editor</button>
+            </div>
+        `;
+    }
+}
+customElements.define('tabs-component', PseudoTab)
