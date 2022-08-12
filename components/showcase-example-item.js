49fed837a0d1a60118070a09cc50e91ff2cc0712
Geoff Bullen <geoff.bullen@gmail.com>
2022-08-12 15:38:16 +1000

    link to index.html

--- components/showcase-example-item.js
+++ components/showcase-example-item.js
@@ -35,8 +35,7 @@ class ShowcaseExampleItem extends HTMLElement {
             }
           </style>
           <div>
-            <a href="/showcase">
-
+            <a href="index.html">
               <h2 class="dashboard-example-h2">
                 <svg width="22" height="22" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M0.292893 8.70711C-0.097631 8.31658 -0.0976311 7.68342 0.292893 7.29289L6.65685 0.928933C7.04738 0.538409 7.68054 0.538409 8.07107 0.928933C8.46159 1.31946 8.46159 1.95262 8.07107 2.34315L2.41421 8L8.07107 13.6569C8.46159 14.0474 8.46159 14.6805 8.07107 15.0711C7.68054 15.4616 7.04738 15.4616 6.65685 15.0711L0.292893 8.70711ZM16 9L1 9L1 7L16 7L16 9Z" fill="#192B80"/>
