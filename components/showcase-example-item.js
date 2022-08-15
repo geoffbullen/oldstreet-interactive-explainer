53f07067ccc942712d444860b56d2f92a6388a2f
Jeff Zhang <jeff@wallsync.net>
2022-08-15 17:27:37 +1000

    styling

--- components/showcase-example-item.js
+++ components/showcase-example-item.js
@@ -18,58 +18,84 @@ class ShowcaseExampleItem extends HTMLElement {
             })
         }
         this.innerHTML = `
-          <style>
-            .dashboard-example-h2{
-              color:black;
-              font-size:38px;
+        <style>
+            .dashboard-example-h2 {
+                display: flex;
             }
-            .dashboard-example-h3{
-                color:black;
-                font-size:28px;
-                margin-top:50px;
+            
+            .dashboard-example-h2>h2 {
+                color: black;
+                font-size: 38px;
             }
-
+            
+            .dashboard-example-h3 {
+                color: black;
+                font-size: 28px;
+                margin-top: 50px;
+            }
+            
             .chart-container {
-              position:relative;
-              display:flex;
-              height: 1880px;
+                position: relative;
+                display: flex;
+                height: 1880px;
             }
+            
             .chart-loading-container {
-                position:absolute;
-                top:0;
-                left:0;
-                width:100%;
-                height:400px;
-                z-index:100;
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                background-color: rgba(236, 236, 236, 0.9);
+                z-index: 100;
             }
-            .iframe-container{
-                position:absolute;
-                top:0px;
-                left:0px;
-                width:100%;
-                height:100%;
+            
+            .chart-loading-container>div {
+                color: #192B80;
+                text-align: center;
             }
-            @keyframes rotate {
-                0% {
-                  transform: rotate(0deg)
-                }
-                100% {
-                  transform: rotate(360deg)
-                }
+            
+            .iframe-container {
+                position: absolute;
+                top: 0px;
+                left: 0px;
+                width: 100%;
+                height: 100%;
             }
-            .scaled-iframe{
-                top:300px !important
+            
+            .scaled-iframe {
+                top: 300px !important
             }
-          </style>
-          <div>
-            <a href="index.html">
-              <h2 class="dashboard-example-h2">
-                <svg width="22" height="22" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M0.292893 8.70711C-0.097631 8.31658 -0.0976311 7.68342 0.292893 7.29289L6.65685 0.928933C7.04738 0.538409 7.68054 0.538409 8.07107 0.928933C8.46159 1.31946 8.46159 1.95262 8.07107 2.34315L2.41421 8L8.07107 13.6569C8.46159 14.0474 8.46159 14.6805 8.07107 15.0711C7.68054 15.4616 7.04738 15.4616 6.65685 15.0711L0.292893 8.70711ZM16 9L1 9L1 7L16 7L16 9Z" fill="#192B80"/>
-                </svg>
-                ${params.name}
-              </h2>
-            </a>
+            
+            .circular-btn {
+                color: #192B80;
+                background-color: #192B8033;
+                width: 48px;
+                height: 48px;
+                border-radius: 50%;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                margin-right: 15px;
+            }
+        </style>
+            <div class="heading">
+                <div class="header-left">
+                    <a href="index.html">
+                    <div class="dashboard-example-h2">
+                        <div class="circular-btn">
+                            <svg width="22" height="22" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M0.292893 8.70711C-0.097631 8.31658 -0.0976311 7.68342 0.292893 7.29289L6.65685 0.928933C7.04738 0.538409 7.68054 0.538409 8.07107 0.928933C8.46159 1.31946 8.46159 1.95262 8.07107 2.34315L2.41421 8L8.07107 13.6569C8.46159 14.0474 8.46159 14.6805 8.07107 15.0711C7.68054 15.4616 7.04738 15.4616 6.65685 15.0711L0.292893 8.70711ZM16 9L1 9L1 7L16 7L16 9Z" fill="#192B80"/>
+                            </svg>
+                        </div>
+                        <h2>${params.name}</h2>
+                    </div>
+                    </a>
+                </div>
+                <call-to-action-btn-stack class="header-right"
+                                secondary-button="features">
+                </call-to-action-btn-stack>
+            </div>
             <div class="chart-container">
                 <div id='loading-spinner'  class="chart-loading-container">
                     <lottie-player src="assets/Custom-Charts-Loader.json"
@@ -78,6 +104,7 @@ class ShowcaseExampleItem extends HTMLElement {
                         style="width: 100px; height: 100px; margin:50px auto;"
                         loop
                         autoplay></lottie-player>
+                    <div>Finding example dashboard...</div>
                 </div>
                 <div class='iframe-container'>
                     <iframe src="https://custom-charts-confluence.oss-playground.com/${params.dashboardUrl}"
